cb354bcd2b0be5c3f670d0fb094c30f51c2a5870
Orontion <orontion@gmail.com>
2016-12-19 02:30:28 +0300

    Накидал комментарии, запилил в модуль процедуру для отображения количества COM-портов в системе, подготовил тело программы к тестам

--- com_module.cpp
+++ com_module.cpp
@@ -2,7 +2,15 @@
 #include <QSerialPort>
 #include <QSerialPortInfo>
 
+//Конструктор класса
 COM_Module::COM_Module()
 {
 
+}
+
+
+//Процедура подсчета количества COM-портов в системе
+int COM_Module::Ports_Count()
+{
+    return QSerialPortInfo::availablePorts().count();
 }
--- main.cpp
+++ main.cpp
@@ -1,8 +1,27 @@
 #include <QCoreApplication>
 
+//Добавим свеженаписанный модуль для тестов
+#include <com_module.h>
+
+//Пара файлов для работы с командной строкой
+#include <QTextStream>
+
+//Главная процедура
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
+
+    //Собственно, модуль для тестов
+    COM_Module TestObj;
+
+    //Входные-выходные потоки
+    QTextStream qtin(stdin);
+    QTextStream qtout(stdout);
+
+    qtout << TestObj.Ports_Count();
+
+    //Не забываем показать изменения в самой командной строке
+    qtout.flush();
 
     return a.exec();
 }
